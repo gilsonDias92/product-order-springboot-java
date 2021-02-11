b1e4e6d19da295977bbb776569ceb357499f4b5c
gilsonDias92 <gilsinho.1992@gmail.com>
2021-02-11 12:38:39 -0300

    Creating entity order and mapping it to database table

--- src/main/java/com/gilsondfilho/productOrder/resources/UserResource.java
+++ src/main/java/com/gilsondfilho/productOrder/resources/UserResource.java
@@ -3,7 +3,6 @@ package com.gilsondfilho.productOrder.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,6 +32,5 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 
 		// return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-
 	}
 }
