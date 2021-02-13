29b59c8378d4f1570410c2aacd42d283db048786
gilsonDias92 <gilsinho.1992@gmail.com>
2021-02-13 16:24:15 -0300

    Implementing user insert

--- src/main/java/com/gilsondfilho/productOrder/resources/UserResource.java
+++ src/main/java/com/gilsondfilho/productOrder/resources/UserResource.java
@@ -1,11 +1,15 @@
 package com.gilsondfilho.productOrder.resources;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +36,18 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 
 		// return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.
+				fromCurrentRequest().
+				path("/{id}").
+				buildAndExpand(obj.getId()).
+				toUri();
+	
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
